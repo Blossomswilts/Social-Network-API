5cca10c07c9e906eb0b5f46361eb589311e0ae56
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-08-12 19:55:27 -0400

    added User model with references

---
+++ models/Users.js
@@ -0,0 +1,47 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true // removes whitespace
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/] // regex for email simple validation
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+// get total count of friends on retrieval
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+}
+);
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+export default User;
