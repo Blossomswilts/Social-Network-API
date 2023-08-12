7de6bd0fce52211fb6715bf8e292706c7b66c607
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-08-12 19:47:37 -0400

    added basic connection.js in config

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+import { connect, connection } from "mongoose";
+
+const connectionString =
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialsDB";
+
+connect(connectionString);
+
+export default connection;
