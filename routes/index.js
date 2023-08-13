275060226cae8c4c9d832440546f3165c6993788
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-08-12 20:08:53 -0400

    added index route page to set up connection

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import router from 'express';
+//api routes
+import apiRoutes from './api';
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).send('<h1>404 Error! Page Not Found</h1>');
+});
+
+export default router;
