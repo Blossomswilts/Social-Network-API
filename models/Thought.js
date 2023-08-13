bf7cfac23d645437d181c93d7108942ef435de85
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-08-12 20:02:32 -0400

    added reactions schema in thoughts model

--- models/Thought.js
+++ models/Thought.js
@@ -35,6 +35,38 @@ ThoughtSchema.virtual('reactionCount').get(function () {
 }
 );
 
+//create a ReactionSchema
+const ReactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment's _id field
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        // set createdAt to a Date value and default it to the current timestamp
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // use a getter method to format the timestamp on query
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
 // create the Thought model using the ThoughtSchema
 const Thought = model('Thought', ThoughtSchema);
 
